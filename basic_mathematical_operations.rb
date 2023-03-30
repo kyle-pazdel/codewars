9d6b093d82f7e50d2b38171b7f781e7d2278e16e
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-03-29 17:02:02 -0700

    Completes 'Basic Mathematical Operations'

---
+++ basic_mathematical_operations.rb
@@ -0,0 +1,13 @@
+# Your task is to create a function that does four basic mathematical operations.
+
+# The function should take three arguments - operation(string/char), value1(number), value2(number).
+# The function should return result of numbers after applying the chosen operation.
+
+def basic_op(operator, value1, value2)
+  return value1.send(operator, value2)
+end
+
+basic_op("+", 4, 7)
+basic_op("-", 15, 18)
+basic_op("*", 5, 5)
+basic_op("/", 49, 7)
