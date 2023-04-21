b80ece929a2d7064469d353ccfb155b91fa31dfb
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-04-21 09:33:18 -0700

    Completes 'DNA to RNA Conversion' in Python

---
+++ Python/dna_to_rna_converstion.py
@@ -0,0 +1,15 @@
+
+# SOLUTION ONE
+# def dna_to_rna(dna):
+#     key = {"G": "G", "C": "C", "A": "A", "T": "U"}
+#     res = []
+#     for c in dna:
+#         res.append(key[c])
+#     return "".join(res)
+
+# SOLUTION TWO
+def dna_to_rna(dna):
+    return "".join(["U" if c == "T" else c for c in dna])
+
+
+print(dna_to_rna("GCAT"))
