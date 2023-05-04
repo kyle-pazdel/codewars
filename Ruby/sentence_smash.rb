e2819855d8e3ad988f487eb4f45a9a179fd19977
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-05-04 14:04:16 -0700

    Completes 'Sentence Smash' in Ruby

---
+++ Ruby/sentence_smash.rb
@@ -0,0 +1,16 @@
+# Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+# SOLUTION ONE
+# def smash(words)
+#   res = ""
+#   words.each { |w| res << w + " " }
+#   return res.chop
+# end
+
+# SOLUTION TWO
+def smash(words)
+  return words.join(" ")
+end
+
+arr = ["hello", "world", "this", "is", "great"]
+smash(arr)
