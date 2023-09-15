a2154a81d54d17f5836d85ad95ed7616e336713c
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-09-14 19:11:39 -0700

    Completes 'sort_and_star' in Python

---
+++ Python/sort_and_star.py
@@ -0,0 +1,8 @@
+def two_sort(array):
+    return '***'.join(sorted(array)[0])
+
+
+print(two_sort(["bitcoin", "take", "over", "the",
+      "world", "maybe", "who", "knows", "perhaps"]))
+print(two_sort(["Lets", "all", "go", "on",
+      "holiday", "somewhere", "very", "cold"]))
