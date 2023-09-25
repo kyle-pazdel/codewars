785efce1ffebdd9e100e7f2d8f26a1978b755004
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-09-25 10:53:59 -0700

    Refactors solution for playiing_with_digits in Python

--- Python/playing_with_digits.py
+++ Python/playing_with_digits.py
@@ -1,12 +1,22 @@
-def dig_pow(a, pow):
+# SOLUTION ONE
+def dig_pow(a, p):
     res = 0
     for i in [*str(a)]:
-        res += int(i)**pow
-        pow += 1
+        res += int(i)**p
+        p += 1
+    return res / a if res % a == 0 else -1
+
+# SOLUTION TWO
+
+
+def dig_pow(a, p):
+    res = 0
+    for i, d in enumerate(str(a)):
+        res += pow(int(d), p+i)
     return res / a if res % a == 0 else -1
 
 
 print(dig_pow(89, 1))
-print(dig_pow(92, 1))
-print(dig_pow(695, 2))
-print(dig_pow(46288, 3))
+# print(dig_pow(92, 1))
+# print(dig_pow(695, 2))
+# print(dig_pow(46288, 3))
