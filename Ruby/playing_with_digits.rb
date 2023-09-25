bd28237debcb9b44e85a5a112ffe9648a0b4d61a
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-09-25 10:34:52 -0700

    Refactors solution for playing_with_digits in Ruby

--- Ruby/playing_with_digits.rb
+++ Ruby/playing_with_digits.rb
@@ -1,3 +1,4 @@
+# SOLUTION ONE
 def dig_pow(a, pow)
   res = 0
   arr = a.to_s.split(//).map{|d| d.to_i}
@@ -5,10 +6,17 @@ def dig_pow(a, pow)
     res+=d**pow
     pow+= 1
   end
-  return res / a != 0 ? res / a : -1
+  return res % a == 0 ? res / a : -1
+end
+
+# SOLUTION TWO
+def dig_pow(a, pow)
+  res = a.to_s.split(//).map.with_index{|d, i| d.to_i ** (pow + i)}.reduce(:+)
+  return res % a == 0 ? res / a : -1
 end
 
 p dig_pow(89, 1)
 p dig_pow(92, 1)
 p dig_pow(695, 2)
 p dig_pow(46288, 3)
+p dig_pow(12643, 50)
