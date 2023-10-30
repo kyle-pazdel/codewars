fa3d970ed260262f2030e032d3d7c1607679cc7a
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-10-30 16:05:19 -0700

    WIP conecutive_strings in Python

---
+++ Python/consecutive_strings.py
@@ -0,0 +1,20 @@
+def longest_consec(strarr, k):
+    longest = ""
+    i = 0
+    while i < len(strarr) - k:
+        consec = ""
+        for j in range(0, 3):
+            consec += strarr[i + j]
+        if len(consec) >= len(longest):
+            longest = consec
+        i += 1
+    return longest
+
+
+# print(longest_consec(["zone", "abigail", "theta", "form", "libe", "zas"], 2))
+# # => "abigailtheta"
+print(longest_consec(["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx",
+      "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"], 1))
+# => "oocccffuucccjjjkkkjyyyeehh"
+# print(longest_consec([], 3))
+# # => ""
