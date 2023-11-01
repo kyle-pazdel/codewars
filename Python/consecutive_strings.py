52e4cde6a877ffa4047e79528dc33a95c2001314
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-11-01 11:01:43 -0700

    Completes consecutive_strings in Python

--- Python/consecutive_strings.py
+++ Python/consecutive_strings.py
@@ -1,14 +1,27 @@
+# # SOLUTION ONE
+# def longest_consec(strarr, k):
+#     longest = ""
+#     i = 0
+#     if k > 0 and len(strarr) >= k:
+#         while i <= len(strarr) - k:
+#             consec = ""
+#             for j in range(0, k):
+#                 consec += strarr[i + j]
+#             if len(consec) >= len(longest):
+#                 longest = consec
+#             i += 1
+#     return longest
+
+# SOLUTION TWO
 def longest_consec(strarr, k):
-    longest = ""
-    i = 0
-    while i <= len(strarr) - k:
-        consec = ""
-        for j in range(0, k):
-            consec += strarr[i + j]
-        if len(consec) >= len(longest):
-            longest = consec
-        i += 1
-    return longest
+    result = ""
+    if k > 0 and len(strarr) >= k:
+        for index in range(len(strarr) - k + 1):
+            s = ''.join(strarr[index: index + k])
+            if len(s) > len(result):
+                result = s
+
+    return result
 
 
 print(longest_consec(["zone", "abigail", "theta", "form", "libe", "zas"], 2))
